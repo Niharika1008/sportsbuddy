3fbf4b862fab86f9485f7345d3f027faeabfb04e
Niharika Chunchu <niharikachunchu10@gmail.com>
2025-09-01 18:58:50 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA5urPILuzjC2u0j7nWwus9g0njV4pLy08",
-    authDomain: "sports-buddy-app-37fff.firebaseapp.com",
-    projectId: "sports-buddy-app-37fff",
-    storageBucket: "sports-buddy-app-37fff.firebasestorage.app",
-    messagingSenderId: "108021286167",
-    appId: "1:108021286167:web:302cab2bf101d9441fe352"
+    REACT_APP_FIREBASE_API_KEY="YOUR_API_KEY"
+    REACT_APP_FIREBASE_AUTH_DOMAIN="YOUR_AUTH_DOMAIN"
+    REACT_APP_FIREBASE_PROJECT_ID="YOUR_PROJECT_ID"
+    REACT_APP_FIREBASE_STORAGE_BUCKET="YOUR_STORAGE_BUCKET"
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID="YOUR_MESSAGING_SENDER_ID"
+    REACT_APP_FIREBASE_APP_ID="YOUR_APP_ID"
 
 };
 
